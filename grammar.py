2c32ab092e8096bb37bc1273314e018402083c98
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-03-08 14:40:05 -0500

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -10,10 +10,9 @@ class Tree:
     def __init__(self, key: str) -> None:
         self.key = key
         self.nodes = []
-        self.rules = ''
 
     def add_node(self, key: str) -> None:
-        self.nodes.append(Tree(key))    
+        self.nodes.append(Tree(key))
 
 
 def build_tree(node: Tree, rules: dict) -> None:
@@ -32,9 +31,9 @@ def build_tree(node: Tree, rules: dict) -> None:
 
 
 # calls _generate and returns generated sentence as a string
-def random_generator(root: Tree, interations: int) -> None:
+def sentence_generator(root: Tree, interations: int) -> None:
     # recursive nested function populates terminals with terminal values
-    def generate(node: Tree, terminals: list) -> bool:
+    def fill_terminals(node: Tree, terminals: list) -> bool:
         # if node has children
         if len(node.nodes) > 0:
             # if node is a compound rule
@@ -42,18 +41,18 @@ def random_generator(root: Tree, interations: int) -> None:
                 # for each rule in the compound
                 for child in node.nodes:
                     # recursive step for each child node
-                    if generate(child, terminals):
+                    if fill_terminals(child, terminals):
                         terminals.append(child.key)
             else:
                 # recursive step for random choice
-                if generate(rand := random.choice(node.nodes), terminals):
+                if fill_terminals(rand := random.choice(node.nodes), terminals):
                     terminals.append(rand.key)
         else:
             return True
 
-    print('\nRandomly generated sentences with a tree:')
+    print('\nRandomly generated sentences:')
     for i in range(interations):
-        generate(root, sentence := [])
+        fill_terminals(root, sentence := [])
         # formats sentence as string
         print(' '.join(sentence).capitalize() + '.')
 
@@ -65,4 +64,4 @@ rule_dict = {
 }
 rule_tree = Tree('S')
 build_tree(rule_tree, rule_dict)
-random_generator(rule_tree, 10)
+sentence_generator(rule_tree, 10)
