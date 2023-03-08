dfd2c2977f611e491205a6b8c1b43b43ea660247
jarvisluke <123421032+jarvisluke@users.noreply.github.com>
2023-03-08 14:22:48 -0500

    Update grammar.py
    Moved _generate() to random_generator() as generate() to improve readability.
    Fixed generate() to allow for more types of cfgs.
    Removed stack-based cfg implementation.

--- grammar.py
+++ grammar.py
@@ -1,82 +1,68 @@
+"""
+Luke Jarvis
+3/8/23
+"""
 import random
 
-rules = {'S': ['NP VP'], 'NP': ['Det N'], 'VP': ['V NP'], 'Det': ['the', 'a'], 'N': ['dog', 'cat', 'bird'],
-         'V': ['chased', 'ate', 'sang']}
+rules = {
+    'S': ('NP VP',), 'NP': ('Det N', 'Det Adj N'), 'VP': ('V NP', 'V Adv Pre NP', 'V'), 'Det': ('the', 'a', 'my'),
+    'N': ('dog', 'cat', 'bird'), 'V': ('chased', 'ate', 'sang', 'barked'), 'Adj': ('fat', 'lazy', 'red'),
+    'Adv': ('quickly', 'quietly', 'loudly', 'rudely'), 'Pre': ('after', 'around', 'at')
+}
 
 
 class Tree:
 
-    def __init__(self, rule):
-        self.rule = rule
+    def __init__(self, key: str) -> None:
+        self.key = key
         self.nodes = []
 
-    def add_node(self, rule) -> None:
-        self.nodes.append(Tree(rule))
+    def add_node(self, key: str) -> None:
+        self.nodes.append(Tree(key))
 
 
-def build_tree(node) -> None:
-    rule = node.rule
+def build_tree(node: Tree) -> None:
+    key = node.key
     # checks if node's rule is in rules
-    if rule in rules:
-        for child in rules.get(rule):
+    if key in rules:
+        for child in rules.get(key):
             node.add_node(child)
     # checks if node's rule is concatenated
-    elif ' ' in rule:
-        for child in rule.split(' '):
+    elif ' ' in key:
+        for child in key.split(' '):
             node.add_node(child)
     for child in node.nodes:
         # recursive step build_tree for each child
         build_tree(child)
 
 
-# should never be called outside of random_generator_tree,
-# fills ls with terminal values
-def _generate(node, ls) -> None | bool:
-    # checks if node is not terminal
-    if len(node.nodes) > 0:
-        for child in node.nodes:
-            # recursive step to choose a terminal element
-            if _generate(child, ls):
-                # selects random child node
-                ls.append(random.choice(node.nodes).rule)
-                break
-    else:
-        return True
-
-
 # calls _generate and returns generated sentence as a string
-def random_generator_tree(node) -> str:
-    sentence_ls = []
-    _generate(node, sentence_ls)
-    # formats list as string
-    return ' '.join(sentence_ls).capitalize()+'.'
-
-
-# generates a correct sentence from a context-free grammar
-def random_generator_stack() -> str:
-    terminal = []
-    non_terminal = ['S']
-    while len(non_terminal) > 0:
-        val = rules.get(non_terminal.pop())
-        # random element from val
-        temp = random.choice(val)
-        if ' ' in temp:
-            non_terminal.extend(temp.split(' '))
+def random_generator(root: Tree, interations: int) -> None:
+    # recursive nested function populates terminals with terminal values
+    def generate(node: Tree, terminals: list) -> bool:
+        # if node has children
+        if len(node.nodes) > 0:
+            # if node is a compound rule
+            if ' ' in node.key:
+                # for each rule in the compound
+                for child in node.nodes:
+                    # recursive step for each child node
+                    if generate(child, terminals):
+                        terminals.append(child.key)
+            else:
+                # recursive step for random choice
+                if generate(rand := random.choice(node.nodes), terminals):
+                    terminals.append(rand.key)
         else:
-            terminal.append(temp)
-    # formats reversed list as string
-    return ' '.join(terminal[::-1]).capitalize()+'.'
+            return True
+
+    print('\nRandomly generated sentences with a tree:')
+    for i in range(interations):
+        generate(root, sentence := [])
+        # formats sentence as string
+        print(' '.join(sentence).capitalize() + '.')
 
 
 rule_tree = Tree('S')
 build_tree(rule_tree)
-
-
-print('\nRandomly generated sentences with a tree:')
-for i in range(5):
-    print(random_generator_tree(rule_tree))
-
-
-print('\nRandomly generated sentences with a stack:')
-for i in range(5):
-    print(random_generator_stack())
+random_generator(rule_tree, 10)
